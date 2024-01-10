842c8628a2f6a3a956e7ee215ea76add5e1152cb
1337encrypted <smallaszero@gmail.com>
2024-01-10 19:06:23 +0530

    refactor

--- src/src.ino
+++ src/src.ino
@@ -1,7 +1,6 @@
 #include <Wire.h>
 #include "U8g2lib.h"
-  
- 
+
 // '1337-logo', 3x47px
 const unsigned char epd_bitmap_1337_logo [] PROGMEM = {
 	0x00, 0x00, 0x00, 0xe6, 0xee, 0x00, 0x84, 0xa8, 0x00, 0xc4, 0x8c, 0x00, 0x84, 0x88, 0x00, 0xee, 
@@ -83,37 +82,51 @@ void setup()
 {
   menuList.setColorIndex(1);  // set the color to white
   menuList.begin();
-  menuList.setBitmapMode(1);                                                             //Create object for oled panel
+  menuList.setBitmapMode(1);   
+  displayMenu();                                                          //Create object for oled panel
 }
 
 void loop() {  
-  displayMenu();
+  
 }
 
 void displayMenu()
 {
   menuList.clearBuffer();  // clear buffer for storing display content in RAM
 
+	
+  displayStaticPage();
+  displayMenuItems(0);
 
-    // draw previous item as icon + label
-  // menuList.setFont(u8g_font_7x14);
-  // menuList.drawStr(25, i+15, menu_items[0]);      
+  menuList.sendBuffer(); // send buffer from RAM to display controller
+}
 
-  // // draw selected item as icon + label in bold font
-  // menuList.setFont(u8g_font_7x14B);    
-  // menuList.drawStr(25, 15+20+2, menu_items[1]);      
-
-  // // draw next item as icon + label
-  // menuList.setFont(u8g_font_7x14);     
-  // menuList.drawStr(25, 15+20+20+2+2, menu_items[2]);  
-
-	menuList.drawXBMP(107, 0, 7, 9, epd_bitmap_bluetooth_icon);
+void displayStaticPage()
+{
+  menuList.drawXBMP(107, 0, 7, 9, epd_bitmap_bluetooth_icon);
 	menuList.drawXBMP(114, 0, 15, 9, epd_bitmap_battery_icon);
   menuList.drawXBMP(112, 58, 17, 6, epd_bitmap_1337_logo);
 	menuList.drawXBMP(45, 0, 37, 9, epd_bitmap_racebot_logo);
 	menuList.drawXBMP(125, 10, 3, 47, epd_bitmap_scrollbar);
 	menuList.drawXBMP(124, 10, 4, 13, epd_bitmap_scrollbar_handle);
-	
 
-  menuList.sendBuffer(); // send buffer from RAM to display controller
+  menuList.drawHLine(0, 9, 128);
+}
+
+void displayMenuItems(uint8_t i)
+{
+  menuList.setFont(u8g2_font_Pixellari_tf);
+  menuList.drawStr(10, 25, menu_items[i%MENU_ITEMS]);      
+  menuList.drawFrame(1,25+2,123,15+2+3);
+  menuList.drawStr(10, 25+15+2, menu_items[(i+1)%MENU_ITEMS]);      
+  menuList.drawStr(10, 25+15+15+2+4, menu_items[(i+2)%MENU_ITEMS]);   
+}
+
+void displaySettingsItems(uint8_t i)
+{
+  menuList.setFont(u8g2_font_Pixellari_tf);
+  menuList.drawStr(10, 25, settings_items[i%SETTINGS_ITEMS]);      
+  menuList.drawFrame(1,25+2,123,15+2+3);
+  menuList.drawStr(10, 25+15+2, settings_items[(i+1)%SETTINGS_ITEMS]);      
+  menuList.drawStr(10, 25+15+15+2+4, settings_items[(i+2)%SETTINGS_ITEMS]);   
 }
